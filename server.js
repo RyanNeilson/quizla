29ab81f9ab97d5eb62c0bac188a85611c1132c0a
User <user@Users-iMac.local>
2022-04-01 17:28:49 -0300

    remove breaking code change

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
-function requireHttps(req, res, next) {
-  if (!req.secure && req.get("x-forwarded-proto")) {
-    return res.redirect("https://" + req.get("host") + req.url);
-  }
-  next();
-}
-app.use(requireHttps);
 app.use(require("./routes/trivia"));
 const path = require("path");
 app.use(express.static(path.join(__dirname, "client", "build")));
