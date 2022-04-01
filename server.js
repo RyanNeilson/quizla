de18ddaffc0bc9ff3a7eb008d081ab0ff0f1acbd
User <user@Users-iMac.local>
2022-04-01 19:38:53 -0300

    add heroku ssl redirect package

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+import sslRedirect from "heroku-ssl-redirect";
 const express = require("express");
 const app = express();
+app.use(sslRedirect());
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
 app.use(require("./routes/trivia"));
 const path = require("path");
 app.use(express.static(path.join(__dirname, "client", "build")));
